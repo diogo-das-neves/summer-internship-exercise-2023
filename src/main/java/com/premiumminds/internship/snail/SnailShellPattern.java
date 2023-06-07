b51bd24b1ff994d8dacdd6e5d535a17be1e2a3dc
Diogo Neves <neves.diogo98@gmail.com>
2023-06-07 10:23:17 +0100

    Added comments; Added test for non square matrix

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -20,21 +20,34 @@ class SnailShellPattern implements ISnailShellPattern {
      */
     private final ExecutorService executor;
 
-    int x, y, left_limit, right_limit, up_limit, down_limit;
-    Queue<String> DirectionsList;
+    private int x, y, left_limit, right_limit, up_limit, down_limit;
+    private final Queue<String> DirectionsList;
 
 
     public SnailShellPattern(){
+        /**
+         * Contructor
+         * */
         this.executor = Executors.newSingleThreadExecutor();
         this.DirectionsList = new LinkedList<>(Arrays.asList("R", "D", "L", "U"));
     }
 
 
-    public boolean checkBoundaries(int currX, int currY) {
-        return left_limit <= currX && currX <= right_limit && up_limit <= currY && currY <= down_limit;
+    public boolean checkBoundaries(int possibleX, int possibleY) {
+        /**
+         * @param possibleX position of the next X
+         * @param possibleY position of the next Y
+         * Checks if the position will be in a valid position within our bondaires
+         * @return True if valid movement, False if not
+         */
+        return left_limit <= possibleX && possibleX <= right_limit && up_limit <= possibleY && possibleY <= down_limit;
     }
 
     public void changeDirection(){
+        /**
+         * Changes directions by putting the Head of the queue in the back and changes the boundaries of the opposite
+         * corner. The pattern is always Right, Down, Left, Up
+         */
         DirectionsList.add(DirectionsList.poll());
         if(DirectionsList.peek().equals("L"))right_limit--;
         if(DirectionsList.peek().equals("R"))left_limit++;
@@ -43,6 +56,10 @@ class SnailShellPattern implements ISnailShellPattern {
     }
 
     public void run() {
+        /**
+         * Funcion that checks the direction which we are moving and moves according to it, whenever we change
+         * directions we always know the next movement as well
+         */
         String direction = DirectionsList.peek();
         if (direction.equals("R")) {
             if (checkBoundaries(x + 1, y)) {
@@ -75,24 +92,12 @@ class SnailShellPattern implements ISnailShellPattern {
         }
     }
 
-    private void updateRightBoundaries(){
-        right_limit--;
-        down_limit--;
-    }
-    private void updateLowerBoundaries(){
-        left_limit++;
-        up_limit++;
-    }
-
     public int[] snailPath(int[][] matrix){
-
-        if (matrix.length != matrix[0].length){
-
-        }
 
         int maxsteps = matrix.length*matrix.length;
         int[] path = new int[maxsteps];
         path[0] = matrix[0][0];
+        /*Setting the boundaries of the matrix*/
         up_limit = 0;
         right_limit = matrix.length-1;
         down_limit = matrix.length-1;
@@ -100,9 +105,6 @@ class SnailShellPattern implements ISnailShellPattern {
 
         x = 0;
         y = 0;
-
-
-
 
         int step = 1;
         while(step<maxsteps){
@@ -113,9 +115,16 @@ class SnailShellPattern implements ISnailShellPattern {
         return path;
     }
 
-
     public Future<int[]> getSnailShell(final int[][] matrix) {
         Future<int[]> result;
+
+        /*If matrix isn't a square one we return an empty array*/
+        for (int[] ints : matrix) {
+            for (int j = 0; j < ints.length; j++) {
+                if (ints.length != matrix[j].length) return executor.submit(() -> new int[0]);
+            }
+        }
+
         result = executor.submit(() -> snailPath(matrix));
         return result;
     }
